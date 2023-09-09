5b3fd80071665ee3da206a0035d071d62ad64a64
Yan Fernandes <yanfernandes404@gmail.com>
2023-09-08 23:22:33 -0300

    Feat: cria componente home e sistema de rotas

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import Home from "./components/Home.tsx";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
