17c6aa6f35c3ca441a553ecff392aaae80bfc505
Yan Fernandes <yanfernandes404@gmail.com>
2023-09-13 22:28:48 -0300

    Format: run npm format

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -12,26 +12,25 @@ function Home() {
     const res = await axios.get(`https://api.github.com/users/${userName}`);
     const data = await res.data;
     console.log(data);
-    
+
     const { avatar_url, login, location, followers, following } = data;
 
-    const userData: UserProps ={
+    const userData: UserProps = {
       avatar_url,
       login,
       location,
       followers,
       following,
     };
-    
+
     setUser(userData);
-    
-    }
+  };
 
   return (
     <div>
       <Search loadUser={loadUser} />
-      
-      {user && <p>{user.login}</p> }
+
+      {user && <p>{user.login}</p>}
     </div>
   );
 }
