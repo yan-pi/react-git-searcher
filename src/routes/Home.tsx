15e88c20bb7aa3e61b2b41f499116c1fa6ed065e
YanDev <yanfernandes404@gmail.com>
2023-09-18 12:36:25 -0300

    feat: implement react toastify for handle error

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,9 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { UserProps } from "../types/user";
 import { useState } from "react";
+
 import Search from "../components/Search";
 import User from "../components/User";
 
+import { ToastContainer, toast } from "react-toastify";
 import axios from "axios";
 
 function Home() {
@@ -11,28 +13,32 @@ function Home() {
 
   const loadUser = async function (userName: string) {
     setUser(null);
-    const res = await axios.get(`https://api.github.com/users/${userName}`);
-    const data = await res.data;
-    console.log(data);
+    try {
+      const res = await axios.get(`https://api.github.com/users/${userName}`);
+      const data = await res.data;
+      console.log(data);
 
-    const { avatar_url, login, location, followers, following } = data;
+      const { avatar_url, login, location, followers, following } = data;
 
-    const userData: UserProps = {
-      avatar_url,
-      login,
-      location,
-      followers,
-      following,
-    };
+      const userData: UserProps = {
+        avatar_url,
+        login,
+        location,
+        followers,
+        following,
+      };
 
-    setUser(userData);
+      setUser(userData);
+    } catch (error) {
+      toast.error("Usuário não encontrado");
+    }
   };
 
   return (
     <div>
       <Search loadUser={loadUser} />
-
       {user && <User {...user} />}
+      <ToastContainer />
     </div>
   );
 }
