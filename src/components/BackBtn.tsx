d40e3a480feff8e6677358eace75345c8445711b
Yan Fernandes <yanfernandes404@gmail.com>
2023-09-20 18:37:35 -0300

    feat: implement styled components

--- src/components/BackBtn.tsx
+++ src/components/BackBtn.tsx
@@ -1,13 +1,29 @@
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 function BackBtn() {
   const navigate = useNavigate();
+  const BackBtn = styled.button`
+    position: absolute;
+    top: 1rem;
+    left: 1rem;
+    padding: 0.5rem 1rem;
+    color: #9da5d1;
+    background-color: #2b3566;
+    cursor: pointer;
+    border: none;
+    border-radius: 5px;
+    opacity: 0.9;
 
-  return(
+  &hover {
+    opacity: 1;
+  }
+  `;
+  return (
     <>
-      <button type="button" onClick={() => navigate(-1)}>
+      <BackBtn type="button" onClick={() => navigate(-1)}>
         voltar
-      </button>
+      </BackBtn>
     </>
   );
 }
