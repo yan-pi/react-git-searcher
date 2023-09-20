b44369f5f758c32769fde045f99c92f752e475e7
Yan Fernandes <yanfernandes404@gmail.com>
2023-09-19 22:17:28 -0300

    wip: implement isLoading useState

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,16 +1,21 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { UserProps } from "../types/user";
 import { useState } from "react";
+
 import Search from "../components/Search";
 import User from "../components/User";
+import Loader from "../components/Loader";
 
 import axios from "axios";
 
 function Home() {
   const [user, setUser] = useState<UserProps | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   const loadUser = async function (userName: string) {
     setUser(null);
+    setIsLoading(true);
+
     const res = await axios.get(`https://api.github.com/users/${userName}`);
     const data = await res.data;
     console.log(data);
