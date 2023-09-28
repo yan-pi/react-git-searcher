04221670d922ff91ed1ed4709776a2999a43f424
YanDev <yanfernandes404@gmail.com>
2023-09-28 17:20:05 -0300

    refactor: adjust app.tsx style for tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
 
-
 function App() {
   return (
     <>
-      <h1>Github Searcher</h1>
+      <div className="flex justify-center items-center">
+        <h1 className="font-bold m-4 text-2xl jusi">Github Searcher</h1>
+      </div>
       <Outlet />
     </>
   );
