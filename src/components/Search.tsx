0f139b384f0d28e990bb9555249732946105c3fb
YanDev <yanfernandes404@gmail.com>
2023-09-28 17:23:29 -0300

    refactor: apply tailwindcss on search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,9 +1,9 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState, KeyboardEvent } from "react";
 import { BsSearch } from "react-icons/bs";
 
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
 
 type SearchProps = {
   loadUser: (userName: string) => Promise<void>;
@@ -19,25 +19,26 @@ function Search({ loadUser }: SearchProps) {
   };
 
   return (
-    <div className=" p-8 rounded-lg flex flex-col items-center gap-4 m-4">
-    <h2 className="text-black">Busque por um usuário:</h2>
-    <p className="text-gray-400">Conheça seus melhores repositórios</p>
-    <div className="flex gap-2">
-      <Input
-        type="text"
-        onChange={(e) => setUserName(e.target.value)}
-        onKeyDown={handleKeyDown}
-        placeholder="Digite o nome do usuário"
-        className="bg-gray-800 p-2 rounded border border-gray-600 text-gray-300"
-      />
-      <Button variant={"primary"}
-        onClick={() => loadUser(userName)}
-        className="bg-blue-500 text-blue-900 p-2 rounded cursor-pointer"
-      >
-        <BsSearch />
-      </Button>
+    <div className="container flex flex-row justify-center items-center">
+      <div className="rid w-full max-w-sm items-center gap-1.5">
+        <Label htmlFor="email">UserName</Label>
+        <div className="flex  gap-2 flex-row">
+          <Input
+            type="text"
+            onChange={(e) => setUserName(e.target.value)}
+            onKeyDown={handleKeyDown}
+            placeholder="Search for a user"
+            className="w-full"
+          />
+          <Button
+            onClick={() => loadUser(userName)}
+            className="bg-gray-200 hover:bg-gray-300"
+          >
+            <BsSearch className="text-slate-950" />
+          </Button>
+        </div>
+      </div>
     </div>
-  </div>
   );
 }
 
